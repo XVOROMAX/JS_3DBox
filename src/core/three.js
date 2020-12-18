7056e0a1c5396dc88c6df92c1c91c61f1cc387f5
Maxim <xvoromax@mail.ru>
2020-12-18 12:04:20 +0300

    second commit

--- src/core/three.js
+++ src/core/three.js
@@ -3,12 +3,12 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHe
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-var geometry = new THREE.BoxGeometry( 10, 20, 10);
+var geometry = new THREE.BoxGeometry( 10, 10, 10);
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 30;
+camera.position.z = 25;
 
 function render() {
         requestAnimationFrame( render );
